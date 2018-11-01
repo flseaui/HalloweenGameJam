f906c1809c2738974201f02f14f81a7a3080f96e
c1fr1 <c1fr1.stkcps@gmail.com>
2018-11-01 05:54:44 +0000

    lots of stuff

--- src/game/Child.java
+++ src/game/Child.java
@@ -1,13 +1,27 @@
 package game;
 
+import engine.OpenGL.Texture;
+import engine.OpenGL.VAO;
+import org.joml.Matrix4f;
+
+import static game.Shaders.textureShader;
+
 public class Child {
-	public int baseX;
-	public int baseY;
 	public int x;
 	public int y;
-	public float radius;
 	
-	public void vibrate() {
-		
+	public static Texture[] childTex;
+	
+	public Child(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public static void renderParents(Child[] parents, VAO vao) {
+		childTex[Math.abs(3 * MainView.frame / MainView.main.window.fps % 4 - 1)].bind();
+		for (Child p: parents) {
+			textureShader.shaders[0].uniforms[0].set(new Matrix4f(MainView.perspectiveMatrix).translate(p.x * 10f, p.y * 10f, 0));
+			vao.drawTriangles();
+		}
 	}
 }
--- src/game/Map.java
+++ src/game/Map.java
@@ -3,6 +3,7 @@ package game;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Map {
 	
@@ -14,12 +15,13 @@ public class Map {
 	
 	public Child[] children;
 	public Parent[] parents;
-	public Player ghost;
 	
-	public static final int[][] tileReference = {
-			
-	};
+	/*public static final int[][] tileReference = {
 	
+	};*/
+	//0 = floor
+	//1 = wall
+	//2 = void
 	public Map(String path) {
 		
 		try {
@@ -27,12 +29,41 @@ public class Map {
 			int[] array = new int[b.getWidth() * b.getWidth()];
 			b.getRGB(0, 0, b.getWidth(), b.getHeight(), array, 0, b.getWidth());
 			int x = 0;
+			
+			solid = new boolean[b.getWidth()][b.getHeight()];
+			tiles = new int[b.getWidth()][b.getHeight()];
+			ArrayList<Child> childrenTemp = new ArrayList<>();
+			ArrayList<Parent> parentTemp = new ArrayList<>();
+			
 			for (int i = 0; i < array.length; ++i) {
-				solid[i % b.getWidth()][i / b.getHeight()] = array[i] == 0x7f7f7f;
-				
+				//System.out.println(array[i]);
+				solid[i % b.getWidth()][i / b.getHeight()] = array[i] == 0xff7f7f7f;
+				if (array[i] == 0xffff0000) {
+					tiles[i % b.getWidth()][i / b.getHeight()] = 0;
+					parentTemp.add(new Parent(i % b.getWidth(), i / b.getHeight()));
+				} else if (array[i] == 0xff0000ff) {
+					tiles[i % b.getWidth()][i / b.getHeight()] = 0;
+					childrenTemp.add(new Child(i % b.getWidth(), i / b.getHeight()));
+				} else {
+					tiles[i % b.getWidth()][i / b.getHeight()] = array[i];
+				}
+			}
+			
+			children = new Child[childrenTemp.size()];
+			for (int i = 0; i < childrenTemp.size(); ++i) {
+				children[i] = childrenTemp.get(i);
+			}
+			parents = new Parent[parentTemp.size()];
+			for (int i = 0; i < parentTemp.size(); ++i) {
+				parents[i] = parentTemp.get(i);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	/*public static void render() {
+		for (int x = 0; x < width) {
+		
+		}
+	}*/
 }
